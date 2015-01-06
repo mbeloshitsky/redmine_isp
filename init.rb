fa055d16beb6aec7e58a09e1b99bb6f25bc2d7df
Michel Beloshitsky <mbeloshitsky@gmail.com>
2015-01-06 12:19:26 +0500

    Edited init.rb

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :redmine_isp do
   name 'Redmine Isp plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  author 'Michel Beloshitsky'
+  description 'Plugin, that adds to redmine Service Provider capabilities (tariffs, services, usage data)'
+  version '0.0.2'
+  url 'http://github.com/mbeloshitsky/redmine_isp'
+  author_url 'http://mbeloshitsky.github.io'
 end
