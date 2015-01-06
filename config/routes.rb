80e1042e5e5c61049c3bcd25b4fe2c5c36dc8055
Michel Beloshitsky <mbeloshitsky@gmail.com>
2015-01-06 13:30:45 +0500

    More GET routes to get things work

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@
 get    	'isp', 							:to => 'redmine_isp_admin#index'
 get    	'isp/links', 					:to => 'redmine_isp_admin#links'
 
+get   	'isp/add',  					:to => 'redmine_isp_admin#add_service'
 post   	'isp/add',  					:to => 'redmine_isp_admin#add_service'
-put    	'isp/:id',  					:to => 'redmine_isp_admin#edit_service'
-delete 	'isp/:id',  					:to => 'redmine_isp_admin#del_service'
+get     'isp/service/:id',				:to => 'redmine_isp_admin#edit_service'
+put    	'isp/service/:id',  			:to => 'redmine_isp_admin#edit_service'
+delete 	'isp/service/:id',  			:to => 'redmine_isp_admin#del_service'
 
 post   	'project/:id/add_isp/:sid',  	:to => 'redmine_isp_admin#add_service_link'
 put    	'project/:id/edit_isp/:sid', 	:to => 'redmine_isp_admin#edit_service_link'
