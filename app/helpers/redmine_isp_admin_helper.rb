879823579872e1dc5f60f87d7c237dd27bf2f752
Michel Beloshitsky <mbeloshitsky@gmail.com>
2015-01-06 14:00:24 +0500

    Fixes for work

--- app/helpers/redmine_isp_admin_helper.rb
+++ app/helpers/redmine_isp_admin_helper.rb
@@ -1,5 +1,9 @@
 module RedmineIspAdminHelper
 	def risp_services_path(service)
-		"xxx"
+		if not service.id
+			url_for :controller => 'redmine_isp_admin', :action => 'add_service'
+		else
+			url_for :controller => 'redmine_isp_admin', :action => 'edit_service', :id => service.id
+		end
 	end
 end
