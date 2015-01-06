82e3e33fb0e2cb20c2f3b2c0b1fe4a845db49827
Michel Beloshitsky <mbeloshitsky@gmail.com>
2015-01-06 14:02:25 +0500

    Getting things work

--- app/helpers/redmine_isp_admin_helper.rb
+++ app/helpers/redmine_isp_admin_helper.rb
@@ -1,9 +1,5 @@
 module RedmineIspAdminHelper
 	def risp_services_path(service)
-		if not service
-			url_for :controller => 'redmine_isp_admin', :action => 'add_service'
-		else
-			url_for :controller => 'redmine_isp_admin', :action => 'edit_service', :id => service.id
-		end
+		url_for :controller => 'redmine_isp_admin', :action => 'add_service'
 	end
 end
